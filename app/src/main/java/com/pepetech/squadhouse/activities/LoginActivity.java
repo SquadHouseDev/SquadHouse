83f96e88c1486b46445d9ecfbe998f8b9f312d5e
Henry Lao <hclao1995@gmail.com>
2021-03-27 20:57:05 -0700

    Add update for login screen core functionality

--- app/src/main/java/com/pepetech/squadhouse/activities/LoginActivity.java
+++ app/src/main/java/com/pepetech/squadhouse/activities/LoginActivity.java
@@ -1,16 +1,80 @@
 package com.pepetech.squadhouse.activities;
 
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 import com.pepetech.squadhouse.R;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    public static final String TAG = "LoginActivity";
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+    private Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+//        if (ParseUser.getCurrentUser() != null) {
+//            goToMainActivity();
+//        }
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick login button");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username, password);
+            }
+        });
+    }
+
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "Attempting to login user: " + username);
+        // TODO: nav to main activity if the user has signed in properly
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    // TODO: better error handling via feedback to user
+                    Log.e(TAG, "Issue with login", e);
+                    return;
+                }
+                goToMainActivity();
+                Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT);
+            }
+        });
+    }
+
+    private void signoutUser() {
+        ParseUser.logOut();
+        ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
+    }
+
+    private void signupUser() {
+        Log.i(TAG, "Attempting to sign up user");
+
+    }
+
+    private void goToMainActivity() {
+        Intent i = new Intent(this, HomeActivity.class);
+        startActivity(i);
+        finish(); // disable user ability to renavigate after a successful login
     }
 }
