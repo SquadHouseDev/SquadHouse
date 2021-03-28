2785be4a1da642bead581dc788dcbe8c17d832ef
Henry Lao <hclao1995@gmail.com>
2021-03-28 04:45:55 -0700

    Add completion of basic login, logout & profile picture features

--- app/src/main/java/com/pepetech/squadhouse/activities/ProfileActivity.java
+++ app/src/main/java/com/pepetech/squadhouse/activities/ProfileActivity.java
@@ -9,19 +9,29 @@ import com.parse.ParseObject;
 import com.parse.ParseUser;
 import com.pepetech.squadhouse.R;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
+    public static final String TAG = "ProfileActivity";
     AppCompatImageView ivProfile, ivProfileNominator;
     TextView tvFullName, tvUsername, tvFollowers, tvFollowing, tvBiography, tvUserJoinDate, tvNominator;
+    Button btnLogout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         // setup view elements
+        // image views
         ivProfile = findViewById(R.id.ivProfile);
         ivProfileNominator = findViewById(R.id.ivProfileNominator);
+        // text views
         tvFullName = findViewById(R.id.tvFullName);
         tvUsername = findViewById(R.id.tvUsername);
         tvFollowers = findViewById(R.id.tvFollowers);
@@ -29,7 +39,22 @@ public class ProfileActivity extends AppCompatActivity {
         tvBiography = findViewById(R.id.tvBiography);
         tvUserJoinDate = findViewById(R.id.tvUserJoinDate);
         tvNominator = findViewById(R.id.tvNominator);
+        // buttons
+        btnLogout = findViewById(R.id.btnLogout);
         setupProfile();
+        setupOnClickListeners();
+    }
+
+    private void setupOnClickListeners() {
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast t = Toast.makeText(getBaseContext(), "Sign out button clicked!", Toast.LENGTH_SHORT);
+                Log.i(TAG, "Sign out button clicked!");
+                signoutUser();
+                goToLoginActivity();
+            }
+        });
     }
 
     private void setupProfile() {
@@ -39,5 +64,15 @@ public class ProfileActivity extends AppCompatActivity {
             Glide.with(this.getBaseContext()).load(image.getUrl()).into(ivProfile);
     }
 
+    private void signoutUser() {
+        ParseUser.logOut();
+        ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
+    }
+
+    private void goToLoginActivity(){
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        finish(); // disable user ability to renavigate after a successful login
+    }
 
 }
