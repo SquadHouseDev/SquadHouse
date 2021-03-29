7f776236f031b758bdbc8f707cffe31f3c56da3e
Henry Lao <hclao1995@gmail.com>
2021-03-29 00:55:49 -0700

    Create SettingsActivity.java

---
+++ app/src/main/java/com/pepetech/squadhouse/activities/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.pepetech.squadhouse.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.pepetech.squadhouse.R;
+
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
