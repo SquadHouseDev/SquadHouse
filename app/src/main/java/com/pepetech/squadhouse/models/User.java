7266e5481ccdb2e8a9b2c2bfd400225d683a53e5
Henry Lao <hclao1995@gmail.com>
2021-03-30 10:26:27 -0700

    Add completion of feature for display follower and following count reflected in backend

--- app/src/main/java/com/pepetech/squadhouse/models/User.java
+++ app/src/main/java/com/pepetech/squadhouse/models/User.java
@@ -5,6 +5,8 @@ public class User {
     public static final String KEY_IMAGE = "image";
     public static final String KEY_LAST_NAME = "lastName";
     public static final String KEY_BIO = "biography";
+    public static final String KEY_FOLLOWING = "following";
+    public static final String KEY_FOLLOWERS = "followers";
 //    public static final String KEY_FIRST_NAME = "firstName";
 //    public static final String KEY_FIRST_NAME = "firstName";
 //    public static final String KEY_FIRST_NAME = "firstName";
