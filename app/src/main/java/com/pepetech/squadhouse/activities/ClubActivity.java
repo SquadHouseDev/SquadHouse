a4d6015f25cc2e4f56a65d4ec30f7fd218313099
Henry Lao <hclao1995@gmail.com>
2021-03-31 09:32:27 -0700

    Add TODO for heterogenous recycler view and test code
    The current code is not linked to by other pages. Instead, it currently reflects clubs followed by the logged in user.

--- app/src/main/java/com/pepetech/squadhouse/activities/ClubActivity.java
+++ app/src/main/java/com/pepetech/squadhouse/activities/ClubActivity.java
@@ -2,22 +2,31 @@ package com.pepetech.squadhouse.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
 import com.parse.ParseUser;
 import com.pepetech.squadhouse.R;
 import com.pepetech.squadhouse.models.Club;
+import com.pepetech.squadhouse.models.User;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
+// TODO: implement heterogenous recycler view
+// 1st section should be the club profile
+// 2nd section should be a recycler of all members with buttons to follow
+// NOTE: current code reflects testing using clubs followed by a user
 public class ClubActivity extends AppCompatActivity {
     public static final String TAG = "ClubActivity";
     TextView tvClubName, tvClubDescription, tvInterests;
     ImageView ivClubImage;
     Club club;
+    ArrayList<ParseObject> allClubs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,21 +40,45 @@ public class ClubActivity extends AppCompatActivity {
         tvInterests = findViewById(R.id.tvInterests);
         tvClubName = findViewById(R.id.tvClubName);
         ivClubImage = findViewById(R.id.ivClubImage);
-
-
         ////////////////////////////////////////////////////////////
         // Query club info and populate
         ////////////////////////////////////////////////////////////
         queryClubProfile();
-
         populateClubProfileElements();
     }
 
     // TODO complete club profile element populating
     private void populateClubProfileElements() {
+        loadClubProfileImage();
+        tvClubDescription.setText(club.getDescription());
+        tvClubName.setText(club.getName());
     }
 
     // TODO complete club profile data querying
     private void queryClubProfile() {
+        // test code needs to be updated to reflect a club a user has clicked on
+        User user = new User();
+        user.User(ParseUser.getCurrentUser());
+        allClubs = user.getClubs();
+//        loadClubProfileImage();
     }
+
+    private void loadClubProfileImage() {
+        // needs to be updated to reflect a club a user has clicked on
+        ParseFile image = null;
+        club = (Club) allClubs.get(0); // TEST CODE
+        try {
+            if (club == null)
+                return;
+            image = club.fetchIfNeeded().getParseFile("image");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        if (image != null)
+            Glide.with(this)
+                    .load(image.getUrl())
+                    .circleCrop()
+                    .into(ivClubImage);
+    }
+
 }
