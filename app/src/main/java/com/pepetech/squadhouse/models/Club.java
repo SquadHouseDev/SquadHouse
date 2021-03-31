ff2ef9f48a7f997973937ac4d6b6549e324ba106
Henry Lao <hclao1995@gmail.com>
2021-03-31 10:00:51 -0700

    Add new CRUD methods to Club API
    - Added setters (they do not update the backend)
    - Added update methods that apply setters and save changes in backend

--- app/src/main/java/com/pepetech/squadhouse/models/Club.java
+++ app/src/main/java/com/pepetech/squadhouse/models/Club.java
@@ -4,6 +4,7 @@ import com.parse.ParseClassName;
 import com.parse.ParseFile;
 import com.parse.ParseObject;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +17,9 @@ public class Club extends ParseObject {
     public static final String KEY_MEMBERS = "members";
     public static final String KEY_INTERESTS = "interests";
 
-
+    ////////////////////////////////////////////////////////////
+    // Getter Methods
+    ////////////////////////////////////////////////////////////
     public String getName() {
         return getString(KEY_NAME);
     }
@@ -37,11 +40,57 @@ public class Club extends ParseObject {
         return getList(KEY_MEMBERS);
 
     }
+
     public List<ParseObject> getInterests() {
         List<ParseObject> rv;
-        rv =  getList(KEY_INTERESTS);
+        rv = getList(KEY_INTERESTS);
         if (rv == null)
             return new ArrayList<ParseObject>();
         return rv;
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////
+    // Setter Methods: need to call saveInBackground on ParseUser in order to effect changes
+    /////////////////////////////////////////////////////////////////////////////////
+    public void setName(String newName) {
+        put(KEY_NAME, newName);
+    }
+
+    public void setDescription(String newDescription) {
+        put(KEY_DESCRIPTION, newDescription);
+    }
+
+    public void setImage(File newImage) {
+        put(KEY_IMAGE, new ParseFile(newImage));
+    }
+
+    public void setImage(ParseFile newImage) {
+        put(KEY_IMAGE, newImage);
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////
+    // Update Methods: automatically calls saveInBackground on ParseUser to effect updates
+    /////////////////////////////////////////////////////////////////////////////////
+    public boolean addInterest(ParseObject interest) {
+        ArrayList<ParseObject> interests = (ArrayList<ParseObject>) getInterests();
+        if (interests.contains(interest)) {
+            return false;
+        }
+        interests.add(interest);
+        put("interests", interests);
+        saveInBackground();
+        return true;
+    }
+
+    public boolean updateImage(File image) {
+        setImage(image);
+        saveInBackground();
+        return true;
+    }
+
+    public boolean updateDescription(String description) {
+        setDescription(description);
+        saveInBackground();
+        return true;
     }
 }
