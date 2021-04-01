50999542cb8ba5fc2b6686768f6913348c1b377a
Henry Lao <hclao1995@gmail.com>
2021-03-31 22:50:03 -0700

    Create SearchActivity.java

---
+++ app/src/main/java/com/pepetech/squadhouse/activities/SearchActivity.java
@@ -0,0 +1,15 @@
+package com.pepetech.squadhouse.activities;
+import com.pepetech.squadhouse.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
