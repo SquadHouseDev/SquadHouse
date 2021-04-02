72346ad14a146d690cb8d2c4faa3fed6df0a78e6
Henry Lao <hclao1995@gmail.com>
2021-04-02 06:35:34 -0700

    Add prototype of the Interest table

--- app/src/main/java/com/pepetech/squadhouse/activities/SearchActivity.java
+++ app/src/main/java/com/pepetech/squadhouse/activities/SearchActivity.java
@@ -9,10 +9,13 @@ import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.pepetech.squadhouse.R;
 import com.pepetech.squadhouse.adapters.HomeFeedAdapter;
+import com.pepetech.squadhouse.adapters.InterestAdapter;
 import com.pepetech.squadhouse.adapters.SearchAdapter;
+import com.pepetech.squadhouse.models.Interest;
 import com.pepetech.squadhouse.models.User;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -20,29 +23,40 @@ import android.opengl.ETC1;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
     public static final String TAG = "SearchActivity";
     List<User> allUsers;
     TextView tvElementsLabel;
-    RecyclerView rvElementsFound;
+    RecyclerView rvElementsFound, rvInterests;
     EditText etSearch;
     Button btnSearch;
-    SearchAdapter adapter;
+    SearchAdapter searchAdapter;
+    InterestAdapter interestAdapter;
     List<User> users;
+    List<Interest> allInterests;
+    private LinkedHashMap<String, List<Interest>> interestsGrouped;
+
     User currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
         currentUser = new User();
         currentUser.User(ParseUser.getCurrentUser());
         users = new ArrayList<>();
@@ -56,13 +70,77 @@ public class SearchActivity extends AppCompatActivity {
         ////////////////////////////////////////////////////////////
         // Setup recycler view
         ////////////////////////////////////////////////////////////
+
         allUsers = new ArrayList<>();
         rvElementsFound = findViewById(R.id.rvElementsFound);
-        adapter = new SearchAdapter(this, allUsers, currentUser);
-        rvElementsFound.setAdapter(adapter);
+        searchAdapter = new SearchAdapter(this, allUsers, currentUser);
+        rvElementsFound.setAdapter(searchAdapter);
         rvElementsFound.setLayoutManager(new LinearLayoutManager(this));
         queryFollowing();
 
+        allInterests = new ArrayList<>();
+//        interestsGrouped = new LinkedHashMap<>();
+
+        rvInterests = findViewById(R.id.rvInterests);
+        interestAdapter = new InterestAdapter(this, allInterests);
+//        interestAdapter = new InterestAdapter(this, interestsGrouped);
+        rvInterests.setAdapter(interestAdapter);
+        rvInterests.setLayoutManager(new GridLayoutManager(this, 2));
+        queryInterests();
+
+    }
+
+    private void queryInterests() {
+        ParseQuery<Interest> query = ParseQuery.getQuery(Interest.class);
+//        query.include("archetype");
+        query.setLimit(200);
+        query.findInBackground(new FindCallback<Interest>() {
+            @Override
+            public void done(List<Interest> interests, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                // 1st version of grouping
+//                for (Interest i : interests) {
+//                    if (interestsGrouped.containsKey(i.getArchetype())) {
+//                        interestsGrouped.get(i.getArchetype()).add(i);
+//                    } else {
+//                        ArrayList<Interest> grouped = new ArrayList<>();
+//                        grouped.add(i);
+//                        interestsGrouped.put(i.getArchetype(), grouped);
+//                    }
+//                }
+                // 2nd method of grouping
+//                for (Interest i : interests) {
+//                    if (!interestsGrouped.containsKey(i.getArchetype())) {
+//                        interestsGrouped.put(i.getArchetype(), new ArrayList<>());
+//                        interestsGrouped.get(i.getArchetype()).add(i);
+//                    } else {
+////                ArrayList<Interest> grouped = new ArrayList<>();
+////                grouped.add(i);
+//                        interestsGrouped.get(i.getArchetype()).add(i);
+//                    }
+//                }
+                allInterests.addAll(interests);
+                Log.i(TAG, "Length of all interests: " + String.valueOf(allInterests.size()));
+
+                interestAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    private void groupInterestsByArchetype(List<Interest> interests) {
+        for (Interest i : interests) {
+            if (!interestsGrouped.containsKey(i.getArchetype())) {
+                interestsGrouped.put(i.getArchetype(), new ArrayList<>());
+                interestsGrouped.get(i.getArchetype()).add(i);
+            } else {
+//                ArrayList<Interest> grouped = new ArrayList<>();
+//                grouped.add(i);
+                interestsGrouped.get(i.getArchetype()).add(i);
+            }
+        }
     }
 
     private void setupOnClickListeners() {
@@ -71,6 +149,7 @@ public class SearchActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Toast.makeText(getBaseContext(), "Search button clicked!", Toast.LENGTH_SHORT).show();
                 Log.i(TAG, "Search button clicked!");
+//                Log.i(TAG, String.valueOf(etSearch.getText()));
                 queryAllUsers(); // DEBUG
             }
         });
@@ -93,23 +172,23 @@ public class SearchActivity extends AppCompatActivity {
                     converted.User(u);
                     allUsers.add(converted);
                 }
-                adapter.notifyDataSetChanged();
+                searchAdapter.notifyDataSetChanged();
             }
         });
     }
 
-    void queryFollowing(){
+    void queryFollowing() {
         List<String> followingIds = ParseUser.getCurrentUser().getList("following");
 
-        if(!followingIds.isEmpty()){
-            for (String id: followingIds) {
+        if (!followingIds.isEmpty()) {
+            for (String id : followingIds) {
                 Log.i(TAG, id);
                 queryUser(id);
             }
         }
     }
 
-    void queryUser(String id){
+    void queryUser(String id) {
         ParseQuery<ParseUser> query = ParseUser.getQuery();
         query.whereEqualTo("objectId", id);
 //        final ParseUser[] rv = {null};
@@ -120,7 +199,7 @@ public class SearchActivity extends AppCompatActivity {
 //                    rv[0] = awesome;
                     User u = new User();
                     u.User(awesome);
-                    Log.i(TAG, u.getLastName() + u.getFirstName() + u.getBiography() + u.getPhoneNumber() );
+                    Log.i(TAG, u.getLastName() + u.getFirstName() + u.getBiography() + u.getPhoneNumber());
                     users.add(u);
                     Log.i(TAG, String.valueOf(users.size()));
                     Log.i(TAG, String.valueOf(users));
@@ -134,7 +213,7 @@ public class SearchActivity extends AppCompatActivity {
 
     }
 
-    void queryByKeywordSearch(){
+    void queryByKeywordSearch() {
 
     }
 }
