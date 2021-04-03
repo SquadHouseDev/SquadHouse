49baf44942b2e212144399e20b7065d50001099e
Henry Lao <hclao1995@gmail.com>
2021-04-03 03:17:18 -0700

    Refactor documentation and accessor methods
    Issues:
    - Poor Documentation
    - Accessor Method Bugs
    
    Resolution:
    - Improved documentation of the Interest class
    - The bug in the accessor methods was tied to fields not having been fetched. Used fetchIfNeeded on the ParseObject to fetch the field from the Parse Server before returning

--- app/src/main/java/com/pepetech/squadhouse/models/Interest.java
+++ app/src/main/java/com/pepetech/squadhouse/models/Interest.java
@@ -5,18 +5,23 @@ import com.parse.ParseClassName;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 
+/**
+ * This is a READ-only class for the table of Interest instances in the Parse Server.
+ * Interest objects should not be modified unless there is need for changing labelling
+ * of a User or a Club's Interests.
+ */
 @ParseClassName("Interest")
 public class Interest extends ParseObject {
     ////////////////////////////////////////////////////////////
     // Table Keys
     ////////////////////////////////////////////////////////////
-    // Parent Label
+    // General Label
     public static final String KEY_ARCHETYPE = "archetype";
-    // Parent Emoji
+    // Emoji associated with General Label
     public static final String KEY_ARCHETYPE_EMOJI = "archetypeEmoji";
-    // Child Name
+    // Specific Label
     public static final String KEY_NAME = "name";
-    // Child Emoji
+    // Emoji associated with specific Label
     public static final String KEY_EMOJI = "emoji";
     ////////////////////////////////////////////////////////////
     // Getters
@@ -35,7 +40,6 @@ public class Interest extends ParseObject {
         }
         return getString(KEY_NAME);
     }
-
     /**
      * Accessor to the Interest archetype. The archetype denotes the category of the Interest instance.
      *
@@ -50,7 +54,6 @@ public class Interest extends ParseObject {
         }
         return getString(KEY_ARCHETYPE);
     }
-
     /**
      * Accessor to the Interest archetypeEmoji. The archetypeEmoji is the visual representation of the Interest archetype.
      *
@@ -67,10 +70,26 @@ public class Interest extends ParseObject {
     }
 
     /**
+     * TODO
+     * Accessor to the Interest emoji. The emoji is the visual representation of the Interest name.
+     *
+     * @return the Interest archetypeEmoji
+     */
+    private String getEmoji() {
+        try {
+            String rv = fetchIfNeeded().getString(KEY_EMOJI);
+            return rv;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return getString(KEY_EMOJI);
+    }
+    /**
      * String representation of the Interest object
+     *
      * @return
      */
-    public String toString(){
+    public String toString() {
         return getArchetype() + "," + getArchetypeEmoji() + "," + getName();
     }
 }
