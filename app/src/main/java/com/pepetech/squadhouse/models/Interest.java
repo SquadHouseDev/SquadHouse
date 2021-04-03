4fa4c90de26bccb155c25838eac47bd299a777ea
Henry Lao <hclao1995@gmail.com>
2021-04-03 03:08:10 -0700

    Refactor documentation and accessor methods

--- app/src/main/java/com/pepetech/squadhouse/models/Interest.java
+++ app/src/main/java/com/pepetech/squadhouse/models/Interest.java
@@ -1,10 +1,9 @@
 package com.pepetech.squadhouse.models;
 
-import com.parse.ParseClassName;
-import com.parse.ParseObject;
-import com.parse.ParseUser;
 
-import java.util.ArrayList;
+import com.parse.ParseClassName;
+import com.parse.ParseException;
+import com.parse.ParseObject;
 
 @ParseClassName("Interest")
 public class Interest extends ParseObject {
@@ -19,25 +18,59 @@ public class Interest extends ParseObject {
     public static final String KEY_NAME = "name";
     // Child Emoji
     public static final String KEY_EMOJI = "emoji";
-
-
     ////////////////////////////////////////////////////////////
     // Getters
     ////////////////////////////////////////////////////////////
+    /**
+     * Accessor to the Interest name.
+     *
+     * @return the Interest name
+     */
+    public String getName() {
+        try {
+            String rv = fetchIfNeeded().getString(KEY_NAME);
+            return rv;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return getString(KEY_NAME);
+    }
 
     /**
-     * READ Interest name
-     * @return
+     * Accessor to the Interest archetype. The archetype denotes the category of the Interest instance.
+     *
+     * @return the Interest archetype
      */
-    public String getName() { return getString(KEY_NAME); }
+    public String getArchetype() {
+        try {
+            String rv = fetchIfNeeded().getString(KEY_ARCHETYPE);
+            return rv;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return getString(KEY_ARCHETYPE);
+    }
+
     /**
-     * READ Interest archetype
-     * @return
+     * Accessor to the Interest archetypeEmoji. The archetypeEmoji is the visual representation of the Interest archetype.
+     *
+     * @return the Interest archetypeEmoji
      */
-    public String getArchetype() { return getString(KEY_ARCHETYPE); }
+    public String getArchetypeEmoji() {
+        try {
+            String rv = fetchIfNeeded().getString(KEY_ARCHETYPE_EMOJI);
+            return rv;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return getString(KEY_ARCHETYPE_EMOJI);
+    }
+
     /**
-     * READ Interest archetype emoji
+     * String representation of the Interest object
      * @return
      */
-    public String getArchetypeEmoji() { return getString(KEY_ARCHETYPE_EMOJI); }
+    public String toString(){
+        return getArchetype() + "," + getArchetypeEmoji() + "," + getName();
+    }
 }
