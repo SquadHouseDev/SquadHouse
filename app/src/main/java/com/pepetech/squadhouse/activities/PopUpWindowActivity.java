d39a7a522721c72db327bf3488709c0ef2a894a7
laothomas01 <laothomas01@gmail.com>
2021-04-02 17:05:16 -0700

    Update Feature Profile
    things completed:
    -pop up window
    -update first name
    -update last name
    -can exit pop up window via tap
    -can access full name via tap
    What needs to still be implemented:
    -no prompt for confirmation
    -username edit

--- app/src/main/java/com/pepetech/squadhouse/activities/PopUpWindowActivity.java
+++ app/src/main/java/com/pepetech/squadhouse/activities/PopUpWindowActivity.java
@@ -1,5 +1,8 @@
 package com.pepetech.squadhouse.activities;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -8,58 +11,105 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.ParseUser;
 import com.pepetech.squadhouse.R;
+import com.pepetech.squadhouse.models.User;
 
-public class PopUpWindowActivity {
+/*
+
+ */
+public class PopUpWindowActivity extends AppCompatActivity {
+    //TAG
+    String TAG = "PopUpWindowActivity";
     //buttons
-    Button buttonUpdateName;
-    Button createAlias;
-    Button cancel;
-    //text views
-    TextView textPrompt;
+
+
+    //essentially this is my OnCreate for the popupWindow
+
+    ParseUser parseUser;
+    User user;
+    protected void OnCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popup_window);
+    }
+
+
     public void displayPopupWindow(final View v) {
 
-        Toast.makeText(v.getContext(),"WOW, THAT SHIT IS POPPIN",Toast.LENGTH_SHORT).show();
+/*
+            This code needs some better rework and clean up
+ */
         //create a view object through inflater
-        LayoutInflater inflater = (LayoutInflater) v.getContext().getSystemService(v.getContext().LAYOUT_INFLATER_SERVICE);
-        View popupView = inflater.inflate(R.layout.popupwindow,null);
-        //Specify the length and width through constants
-        int width = LinearLayout.LayoutParams.MATCH_PARENT;
-        int height = LinearLayout.LayoutParams.MATCH_PARENT;
-        //Make items outside of popupwindow inactive
-        boolean focusable = true;
-        //create a window with our parameters
-       final PopupWindow pw = new PopupWindow(popupView,width,height,focusable);
-        //SET LOCATION OF THE WINDOW ON THE SCREEN
-        pw.showAtLocation(v, Gravity.CENTER,0,0);
-        //INITIALIZE THE ELEMENTS OF OUR WINDOW
-         textPrompt    = popupView.findViewById(R.id.tvPrompt);
-         buttonUpdateName = popupView.findViewById(R.id.updateNameButton);
-        createAlias = popupView.findViewById(R.id.createAliasButton);
-        cancel = popupView.findViewById(R.id.CancelButton);
-        buttonUpdateName.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //As an example, display the message
-                Toast.makeText(v.getContext(), "Wow, popup action button", Toast.LENGTH_SHORT).show();
 
-            }
-        });
+
+        //Specify the length and width through constants
+
+        //Make items outside of popupwindow inactive
+
+        //create a window with our parameters
+
+        //SET LOCATION OF THE WINDOW ON THE SCREEN
+
+        //unwrap parsed user
+//        parseUser = ParseUser.getCurrentUser();
+//        user= new User(parseUser);
+
+
+
+        /*
+
+        IMPORTANT TO IMPLEMENT FIRST
+
+        */
+
+
         //Handle clicking on the inactive zone(the outside area) of the window
         //Handler for clicking on the inactive zone of the window
 
-        popupView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
 
-                //Close the window when clicked
-                pw.dismiss();
-                return true;
-            }
-        });
+
 
     }
+//    public void setup_Popup_Window_On_Click_Listeners() {
+//        buttonUpdateName.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //As an example, display the message
+//                Log.i(TAG,"UPDATE BUTTON CLICKED");
+//                        //------------------TEST DATA-------------
+////                        System.out.println(parseUser);
+////                        System.out.println(user.getFirstName() + user.getLastName());
+////                        user.updateFirstName("cheese");
+////                        System.out.println(user.getFirstName());
+//                        //--------------------------------
+//                //GO TO UpdateFullnameActivity
+//                Intent i = new Intent(PopUpWindowActivity.this,UpdateFullNameActivity.class);
+//                startActivity(i);
+//
+//            }
+//        });
+//        /*
+//
+//        IMPORT TO IMPLEMENT FIRST
+//
+//         */
+//        cancel.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+//        createAlias.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+//    }
 
 }
