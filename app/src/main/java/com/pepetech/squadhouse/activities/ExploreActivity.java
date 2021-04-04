4d2682c7c96111982238daad8bcce333f3e1056c
Henry Lao <hclao1995@gmail.com>
2021-04-04 03:14:22 -0700

    Revert "Merge branch 'feature-club-page' into feature-search-page-and-follow"
    This reverts commit dcb058cd15daacfcb93d3a0cb79c3d1885cb3e51, reversing
    changes made to 13ffbd9570078249e444335b685b57b5d1cd5b1c.

--- app/src/main/java/com/pepetech/squadhouse/activities/ExploreActivity.java
+++ app/src/main/java/com/pepetech/squadhouse/activities/ExploreActivity.java
@@ -90,7 +90,7 @@ public class ExploreActivity extends AppCompatActivity {
         rvInterests.setLayoutManager(new GridLayoutManager(this, 2));
         exploreInterestAdapter = new ExploreInterestAdapter(this, interestsGrouped);
         rvInterests.setAdapter(exploreInterestAdapter);
-
+        
         // query data for populating with adapters
         queryFollowing();
         queryAndGroupInterestsByArchetype();
@@ -363,4 +363,6 @@ public class ExploreActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
--- app/src/main/java/com/pepetech/squadhouse/activities/ViewAClubActivity.java
+++ app/src/main/java/com/pepetech/squadhouse/activities/ViewAClubActivity.java
@@ -27,7 +27,7 @@ public class ViewAClubActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_club_profile);
+        setContentView(R.layout.activity_club);
 
         ////////////////////////////////////////////////////////////
         // Initialize club page variables
