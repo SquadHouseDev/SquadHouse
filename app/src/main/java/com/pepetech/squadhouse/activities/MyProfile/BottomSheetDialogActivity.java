38a30e9e7e593422ec380185ac54de1e6c700611
Henry Lao <hclao1995@gmail.com>
2021-04-07 22:49:58 -0700

    Update BottomSheetDialogActivity.java

--- app/src/main/java/com/pepetech/squadhouse/activities/MyProfile/BottomSheetDialogActivity.java
+++ app/src/main/java/com/pepetech/squadhouse/activities/MyProfile/BottomSheetDialogActivity.java
@@ -1,43 +1,21 @@
 package com.pepetech.squadhouse.activities.MyProfile;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.ImageDecoder;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.parse.ParseUser;
 import com.pepetech.squadhouse.R;
 import com.pepetech.squadhouse.models.User;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-
-import droidninja.filepicker.FilePickerConst;
-import droidninja.filepicker.utils.ContentUriUtils;
-
-import static android.app.Activity.RESULT_OK;
 
 public class BottomSheetDialogActivity extends BottomSheetDialogFragment {
     public static final String TAG = "BottomSheetDialogActivity";
