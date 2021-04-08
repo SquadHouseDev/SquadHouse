5844fad87c4aa1247cce16e1868b3c16c415cce7
Henry Lao <hclao1995@gmail.com>
2021-04-08 03:39:12 -0700

    Create FollowersActivity.java

---
+++ app/src/main/java/com/pepetech/squadhouse/activities/FollowersActivity.java
@@ -0,0 +1,17 @@
+package com.pepetech.squadhouse.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pepetech.squadhouse.R;
+
+
+public class FollowersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_followers);
+    }
+}
