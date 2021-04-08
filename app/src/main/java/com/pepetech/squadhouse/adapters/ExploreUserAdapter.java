5312fbb7e920bbc3b274285474cbce2e213b8ec4
Henry Lao <hclao1995@gmail.com>
2021-04-08 07:10:44 -0700

    Removed curentUser and disabled ability for a User to follow oneself

--- app/src/main/java/com/pepetech/squadhouse/adapters/ExploreUserAdapter.java
+++ app/src/main/java/com/pepetech/squadhouse/adapters/ExploreUserAdapter.java
@@ -17,6 +17,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
 import com.pepetech.squadhouse.R;
 import com.pepetech.squadhouse.activities.ExploreUserActivity;
 import com.pepetech.squadhouse.models.Follow;
@@ -38,10 +40,10 @@ public class ExploreUserAdapter extends RecyclerView.Adapter<ExploreUserAdapter.
     private User currentUser;
     private List<Boolean> allFollowings;
 
-    public ExploreUserAdapter(Context context, List<User> users, User currentUser) {
+    public ExploreUserAdapter(Context context, List<User> users) {
         this.context = context;
         this.allUsers = users;
-        this.currentUser = currentUser;
+//        this.currentUser = currentUser;
 //        queryFollowings();
     }
 
@@ -81,6 +83,7 @@ public class ExploreUserAdapter extends RecyclerView.Adapter<ExploreUserAdapter.
             btnFollow = itemView.findViewById(R.id.btnFollow);
             clProfile = itemView.findViewById(R.id.clProfile);
             wasFollowed = false;
+            currentUser = new User(ParseUser.getCurrentUser());
             follow = null;
         }
 
@@ -118,7 +121,9 @@ public class ExploreUserAdapter extends RecyclerView.Adapter<ExploreUserAdapter.
         private void setupFollowButton(User userElement) {
             // if the user was in a previous state of being followed by the current user
             if (userElement.isFollowed) {
-                Log.i(TAG, userElement.getFirstName() + " is currently followed by " + currentUser.getFirstName());
+                // comment out the following line when debugging FollowersActivity
+//                Log.i(TAG, userElement.getFirstName() + " is currently followed by " + currentUser.getFirstName());
+
                 // show state of followed
                 btnFollow.setText("Following");
                 handleOnFollowButton(userElement);
@@ -142,9 +147,17 @@ public class ExploreUserAdapter extends RecyclerView.Adapter<ExploreUserAdapter.
                     Toast.makeText(context, "Follow button clicked!", Toast.LENGTH_SHORT).show();
                     Log.i(TAG, "Follow button clicked!");
                     // on button click and not followed --> action to follow
-                    if (!userElement.isFollowed) {
+                    if (!userElement.isFollowed && !userElement.getParseUser().getObjectId().equals(currentUser.getParseUser().getObjectId())) {
+                        // safety check for creating a new follow
+                        for (ParseObject o : currentUser.getFollowing()) {
+                            if (o.getObjectId().equals(userElement.getParseUser().getObjectId()))
+                                return;
+                        }
                         btnFollow.setText("Following");
                         // apply addition of a following on the user object
+                        Log.i(TAG, "Creating Follow...");
+                        Log.i(TAG, "From: " + currentUser.getFirstName());
+                        Log.i(TAG, "To: " + userElement.getFirstName());
                         currentUser.follow(userElement.getParseUser());
                         // create the follow object for accessing followers
                         follow = new Follow();
