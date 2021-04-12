153e9c3fb206677834fd9c653604b4fc323c32b7
Henry Lao <hclao1995@gmail.com>
2021-04-12 16:19:05 -0700

    Documentation, demo, minor bug fixes
    Create v1.0.0-home-page-refresh-bug.gif
    
    Renamed for clarity of variable's function
    
    Fix bug that generates multiple explore and future cells
    
    Refactor to use non-DEBUG code
    
    Add documentation for class usage
    
    Create squad-house-room-activity-prototype.uxf
    
    Create squad-house-room-activity-prototype.png
    
    Add documentation

--- app/src/main/java/com/pepetech/squadhouse/activities/FollowingActivity.java
+++ app/src/main/java/com/pepetech/squadhouse/activities/FollowingActivity.java
@@ -6,7 +6,11 @@ import android.os.Bundle;
 
 import com.pepetech.squadhouse.R;
 
-
+/**
+ * Activity that routes using a Hetero Recycler View.
+ * Items should be displayed in the order of Clubs being first followed by
+ * Users. 
+ */
 public class FollowingActivity extends AppCompatActivity {
 
     @Override
