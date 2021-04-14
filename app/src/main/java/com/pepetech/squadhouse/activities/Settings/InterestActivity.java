f01d29ed7ebd8c6f1f61881c9b0f88d33db1adef
Henry Lao <hclao1995@gmail.com>
2021-04-14 09:45:35 -0700

    Fix major bug with polling Interest and defining Interest existence in User's list of Interests

--- app/src/main/java/com/pepetech/squadhouse/activities/Settings/InterestActivity.java
+++ app/src/main/java/com/pepetech/squadhouse/activities/Settings/InterestActivity.java
@@ -3,16 +3,20 @@ package com.pepetech.squadhouse.activities.Settings;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
 import com.pepetech.squadhouse.R;
 import com.pepetech.squadhouse.activities.Settings.adapters.OuterInterestAdapter;
 import com.pepetech.squadhouse.models.Interest;
+import com.pepetech.squadhouse.models.User;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -45,7 +49,7 @@ public class InterestActivity extends AppCompatActivity {
         rvOuterInterest = findViewById(R.id.rvOuterInterest);
         interests = new ArrayList<>();
         outerAdapter = new OuterInterestAdapter(this,
-                interests
+                interests, new User(ParseUser.getCurrentUser())
 //                lhmInterests
         );
         rvOuterInterest.setAdapter(outerAdapter);
@@ -63,7 +67,8 @@ public class InterestActivity extends AppCompatActivity {
      * @param inputInterests
      */
     private void queryAndGroupInterestsByArchetype(RecyclerView.Adapter inputAdapter, List<List<Interest>> inputInterests) {
-//        lhmInterests.clear();
+        // toggle for debug logging
+        boolean DEBUG = false;
         inputInterests.clear();
         Log.i(TAG, "queryAndGroupInterestsByArchetype call");
         ParseQuery<Interest> query = ParseQuery.getQuery(Interest.class);
@@ -76,20 +81,90 @@ public class InterestActivity extends AppCompatActivity {
                     return;
                 }
                 LinkedHashMap<String, List<Interest>> lhmInterests = new LinkedHashMap<>();
+                User u = new User(ParseUser.getCurrentUser());
+                List<Interest> userInterests = u.getInterests();
+                Log.i(TAG, "Before MAP Interest List size: " + userInterests.size());
+                for (Interest i : userInterests) {
+                    Log.i(TAG, i.getObjectId() + " : " + i.getName());
+                }
+                // map archetype to list of interests
                 for (Interest i : interestsQueried) {
                     if (!lhmInterests.keySet().contains(i.getArchetype())) {
+                        // create new empty list
                         lhmInterests.put(i.getArchetype(), new ArrayList<>());
+                        // check if interest is in User's interest list
+                        if (DEBUG) {
+                            Log.i(TAG, "IF start loop objectId: " + i.getName());
+                            Log.i(TAG, "Interest Checked: " + i.getName());
+                        }
+                        for (Interest _ : userInterests) {
+                            if (DEBUG) {
+                                Log.i(TAG, "Interest List size: " + userInterests.size());
+                            }
+                            if (_.getObjectId().equals(i.getObjectId())) {
+                                if (DEBUG) {
+                                    Log.i(TAG, "Create new list " + i.getName() + " is in User's list");
+                                }
+                                i.isSelected = true;
+                                break;
+                            } else {
+                                if (DEBUG) {
+                                    Log.i(TAG, "Create new list " + i.getName() + " is not in User's list");
+                                }
+                                i.isSelected = false;
+                            }
+                        }
+                        if (DEBUG) {
+                            Log.i(TAG, "IF end loop objectId: " + i.getName());
+                        }
+                        // add to newly created list
                         lhmInterests.get(i.getArchetype()).add(i);
-                        Log.i(TAG, "Not in hashmap keys: " + i.getArchetype() + " ==> " + i.toString());    // DEBUG
-                    } else {
-                        Log.i(TAG, "Key exists adding: " + i.toString());   // DEBUG
-                        lhmInterests.get(i.getArchetype()).add(i);
+                        if (DEBUG) {
+                            Log.i(TAG, "Not in hashmap keys: " + i.getArchetype() + " ==> " + i.toString());    // DEBUG}
+                        } else {
+
+                            // check if interest is in User's interest list
+                            if (DEBUG) {
+                                Log.i(TAG, "ELSE start loop objectId: " + i.getName());
+                                Log.i(TAG, "Key exists adding: " + i.toString());   // DEBUG
+                                Log.i(TAG, "Interest Checked: " + i.getName());
+                                Log.i(TAG, "Interest List size: " + userInterests.size());
+                            }
+                            for (Interest _ : userInterests) {
+                                if (_.getObjectId().equals(i.getObjectId())) {
+                                    if (DEBUG) {
+                                        Log.i(TAG, "Add to existing list " + i.getName() + " is in User's list");
+                                    }
+                                    i.isSelected = true;
+                                    break;
+                                } else {
+                                    if (DEBUG) {
+                                        Log.i(TAG, "Add to existing list " + i.getName() + " is not in User's list");
+                                    }
+                                    i.isSelected = false;
+                                }
+                            }
+                            Log.i(TAG, "ELSE end loop objectId: " + i.getName());
+                            // add to existing list
+                            lhmInterests.get(i.getArchetype()).add(i);
+                        }
                     }
-                }
-                for (Map.Entry<String, List<Interest>> entry : lhmInterests.entrySet()) {
-                    inputInterests.add((List<Interest>) entry.getValue());
-                    Log.i(TAG, "inputInterests size: " + String.valueOf(inputInterests.size()));
-                    Log.i(TAG, inputInterests.get(0).get(0).getArchetype());
+                    // add to 2d list
+                    for (Map.Entry<String, List<Interest>> entry : lhmInterests.entrySet()) {
+                        inputInterests.add((List<Interest>) entry.getValue());
+                        Log.i(TAG, "inputInterests size: " + String.valueOf(inputInterests.size()));
+                        Log.i(TAG, inputInterests.get(0).get(0).getArchetype());
+                    }
+                    // DEBUG
+                    if (DEBUG) {
+                        for (List<Interest> i : inputInterests) {
+                            for (Interest j : i) {
+                                if (j.isSelected) {
+                                    Log.i(TAG, j.toString() + " is selected");
+                                }
+                            }
+                        }
+                    }
                 }
                 inputAdapter.notifyDataSetChanged();
             }
