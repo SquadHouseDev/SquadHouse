5bb8907721a03cea253b31905384b9b5ac080b51
Henry Lao <hclao1995@gmail.com>
2021-04-14 12:53:51 -0700

    Create RoomRoute.java

---
+++ app/src/main/java/com/pepetech/squadhouse/models/RoomRoute.java
@@ -0,0 +1,8 @@
+package com.pepetech.squadhouse.models;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+@ParseClassName("RoomRoute")
+public class RoomRoute extends ParseObject {
+}
